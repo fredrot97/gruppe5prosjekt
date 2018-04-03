b1f605b935f8504ac9d76be641a6157aaedb65e9
Redheadkittycat <karingl@stud.ntnu.no>
2018-04-03 21:45:15 +0200

    Logg inn, logg ut og oppdater detaljer fungerer

--- src/app.js
+++ src/app.js
@@ -1,42 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Link, HashRouter, Switch, Route } from 'react-router-dom';
-import { UserService } from './services.js';
-import { userService } from './services.js';
-
-class Menu extends React.Component {
-  render() {
-    return (
-      <div>
-        Opprett bruker: <Link to='/createUser/'>Opprett bruker</Link>
-      </div>
-    );
-  }
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link, HashRouter, Switch, Route } from "react-router-dom";
+import createHashHistory from "history/createHashHistory";
+const history = createHashHistory();
+import { userService } from "./services.js";
 
   class LoginScreen extends React.Component {
   constructor() {
     super();
-
-    this.users = [];
   }
 
-  render() {
+  render()  {
     return (
       <div>
       <h2>Email:</h2>
         <input type="text" ref="inputEmail" />
       <h2>Passord:</h2>
         <input type="text" ref="inputPassword" />
+        <div id="loginError"></div>
           <button ref="userLoginButton">Logg inn</button>
 
         <div>
         <h2>Glemt passord:</h2>
-        <button ref="passwordForgottenButton">Hent passord</button>
+        <Link to="/forgottenPassword/">Hent passord</Link>
         </div>
+      <div>
+        <h2>Opprett bruker:</h2>
+        <Link to="/createUser/">Ny bruker</Link>
+      </div>
       </div>
 
     );
+  }
+  componentDidMount() {
+    this.refs.userLoginButton.onclick = () => {
+      userService.signIn(this.refs.inputEmail.value, this.refs.inputPassword.value, (user) => {
+        if (user.firstName == "No Result") {
+          document.getElementById("loginError").textContent = "Email or password is incorrect.";
+        }
+        else {
+          history.replace("/profile/:" + user.email + "");
+        }
+      })
+    };
   }
   //Framtidige funksjoner:
   //*userLoginButton vil ta deg til UserProfile dersom du har skrevet inn riktig email og Passord
@@ -44,6 +50,197 @@ class Menu extends React.Component {
 }
 
 class UserProfile extends React.Component {
+constructor(props) {
+  super(props); // Call React.Component constructor
+
+  var user = userService.getSignedInUser();
+  this.user = user;
+}
+
+render() {
+  var user = userService.getSignedInUser();
+  this.user = user;
+  console.log(user);
+  console.log("HEY IM RENDERING HERE");
+  return (
+    <div>
+    <div>
+      <button ref="userLogoutButton">Logg ut</button>
+    </div>
+    <div>
+      <Link to="/arrangements/">Arrangementer</Link>
+    </div>
+    <div>
+    <h2>Fornavn: {this.user.firstName}</h2>
+    <h2>Etternavn: {this.user.lastName}</h2>
+
+    <h2>Addresse: {this.user.address}</h2>
+    <h2>Telefonnummer: {this.user.phonenumber}</h2>
+
+
+    <h2>Kompetanse: </h2>
+    <button ref="changeProfileDetailsButton">Endre detaljer</button>
+    </div>
+    </div>
+    );
+  }
+  componentDidMount() {
+ this.refs.userLogoutButton.onclick = () => {
+   userService.userLogOut();
+     history.replace("/");
+    }
+  this.refs.changeProfileDetailsButton.onclick = () => {
+    history.replace("/changeProfile/");
+    }
+  }
+}
+
+class ChangeProfile extends React.Component{
+  constructor(props) {
+    super(props); // Call React.Component constructor
+
+    var user = userService.getSignedInUser();
+    this.user = user;
+  }
+  render() {
+    return (
+      <div>
+      <nav>
+        <ul>
+          <li>
+            <h2>Profil</h2>
+          </li>
+          <li>
+            <Link to="/arrangements/">Arrangementer</Link>
+          </li>
+          <li>
+            <Link to="/otherUsers/">Søk opp medlem</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+      <h2>Fornavn: {this.user.firstName}</h2>
+      <input type="text" ref="changeFirstName" />
+      <h2>Etternavn: {this.user.lastName}</h2>
+      <input type="text" ref="changeLastName" />
+      <h2>Addresse: {this.user.address}</h2>
+      <input type="text" ref="changeAddress" />
+      <h2>Telefonnummer: {this.user.phonenumber}</h2>
+      <input type="text" ref="changePhonenumber" />
+
+
+      <h2>Kompetanse:</h2>
+      <input type="text" ref="changeCompetence" />
+      <button ref="changeUserDetailsButton">Endre detaljer</button>
+      </div>
+      </div>
+      );
+    }
+    componentDidMount() {
+      var user = userService.getSignedInUser();
+      this.user = user;
+   this.refs.changeUserDetailsButton.onclick = () => {
+     userService.changeUserProfile(this.refs.changeFirstName.value, this.refs.changeLastName.value, this.refs.changeAddress.value, this.refs.changePhonenumber.value, this.user.email, this.user.password, (user) => {
+       history.replace("/changeProfileSuccess/");
+     });
+     }
+    }
+}
+//Start på brukerens profil
+class ChangeProfileSuccess extends React.Component {
+  constructor(props) {
+    super(props); // Call React.Component constructor
+
+    var user = userService.getSignedInUser();
+    this.user = user;
+  }
+  render() {
+    return (
+      <div>
+      <div>
+      <h2>Dine endringer er oppdatert!</h2>
+      <h4>Tilbake til din profil:</h4>
+      <button ref="backToProfileButton">Din profil</button>
+      </div>
+      </div>
+    )
+  }
+  componentDidMount() {
+    var user = userService.getSignedInUser();
+    this.user = user;
+    this.refs.backToProfileButton.onclick = () => {
+      history.replace("/profile/:" + user.email + "");
+    }
+  }
+}
+
+class Arrangements extends React.Component {
+  constructor(props) {
+    super(props);
+
+    var user = userService.getSignedInUser();
+    this.user = user;
+  }
+
+  render() {
+    return(
+      <div>
+    <nav>
+      <ul>
+        <li>
+
+        </li>
+        <li>
+          <h2>Arrangementer</h2>
+        </li>
+        <li>
+          <Link to="/OtherUsers/">Søk opp medlem</Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  )
+  }
+}
+
+class UserProfileAdmin extends React.Component {
+constructor(props) {
+  super(props); // Call React.Component constructor
+
+  this.user = {};
+
+  this.email = props.match.params.email;
+}
+
+render() {
+    let listNewUsers = [];
+    for(let newUser of this.newUsers) {
+      listNewUsers.push(
+        <li key={newUser.email}>
+          <Link to={"/NewUser/" + newUser.email}>{newUser.firstName}</Link>
+        </li>
+      );
+    }
+    let listUsers = [];
+    for(let user of this.users) {
+      listUsers.push(
+        <li key={newUser.email}>
+          <Link to={"/UserProfile/" + user.email}>{user.firstName, user.lastName}</Link>
+          <button onClick={() => {
+            console.log("button for user " + user.email + " clicked");
+            service.deactivateUser(user.email);
+            service.getUsers((result) => {
+              this.users = result;
+              this.forceUpdate(); // Rerender component with updated data
+            });
+          }}>x</button>
+        </li>
+      );
+    }
+  }
+}
+
+class ForgottenPassword extends React.Component {
 constructor() {
   super(); // Call React.Component constructor
 
@@ -53,16 +250,26 @@ constructor() {
 render() {
   return (
     <div>
-    <h2>Brukernavn: {this.user.firstName}</h2>
-    <h2>Etternavn: {this.user.lastName}</h2>
-
-    <h2>Addresse: {this.user.address}</h2>
-    <h2>Telefonnummer: {this.user.phonenumber}</h2>
+      <div>
+        <Link to="/">Tilbake til login</Link>
+      </div>
+      <div>
+        <h2>Skriv inn email:</h2>
+        <input type="text" ref="passwordEmail" />
+        <button ref="newPasswordRequestButton">Få nytt passord</button>
+        <h3>Viktig: dette vil tilbakestille ditt nåværende passord!</h3>
+      </div>
     </div>
     );
   }
+componentDidMount() {
+  this.refs.newPasswordRequestButton.onclick = () => {
+    service.newPassword(this.refs.passwordEmail.value, (user) => {
+      history.replace("/passwordConfirmation/");
+    });
+  }
 }
-//Start på brukerens profil
+}
 
 class CreateUser extends React.Component {
   constructor(props) {
@@ -76,21 +283,25 @@ class CreateUser extends React.Component {
   render() {
     return (
       <div>
-      <h1>Opprett-bruker-side</h1>
+        <div>
+          <Link to="/">Tilbake til login</Link>
+        </div>
+        <div>
+          <h1>Opprett-bruker-side</h1>
 
-        <h2>Fornavn:</h2>
-          <input type="text" ref="nFirstname" />
-        <div id="fnameError"></div>
-        <h2>Etternavn:</h2>
-          <input type="text" ref="nLastname" />
-        <div id="lnameError"></div>
+          <h2>Fornavn:</h2>
+            <input type="text" ref="nFirstname" />
+            <div id="fnameError"></div>
+            <h2>Etternavn:</h2>
+            <input type="text" ref="nLastname" />
+            <div id="lnameError"></div>
 
-        <h2>Address:</h2>
-          <input type="text" ref="nAddress" />
-        <div id="addressError"></div>
+            <h2>Address:</h2>
+            <input type="text" ref="nAddress" />
+            <div id="addressError"></div>
 
-        <h2>Epost:</h2>
-          <input type="text" ref="nEmail1" />
+            <h2>Epost:</h2>
+            <input type="text" ref="nEmail1" />
         <div id="emailError1"></div>
         <h2>Bekreft epost:</h2>
           <input type="text" ref="nEmail2" />
@@ -110,6 +321,7 @@ class CreateUser extends React.Component {
         <div id="addUserError"></div>
         <p> </p>
         <button ref="addUserButton">Opprett bruker</button>
+        </div>
       </div>
     );
   }
@@ -139,7 +351,7 @@ class CreateUser extends React.Component {
       } else {
         document.getElementById("addressError").textContent = "";
       }
-      //Sjekker om innskrevet addresse boksen er tom
+
       if(this.refs.nEmail1.value != this.refs.nEmail2.value) {
         isValidInput = false;
         document.getElementById("emailError2").textContent = "Emails do not match.";
@@ -152,6 +364,11 @@ class CreateUser extends React.Component {
       } else {
         document.getElementById("emailError1").textContent = "";
         document.getElementById("emailError2").textContent = "";
+      }
+      userService.checkEmail(this.refs.nEmail1.value, () => {});
+      if(userService.isEmailTaken) {
+        document.getElementById("emailError1").textContent = "Email is already taken asshat";
+        isValidInput = false;
       }
       //Sjekker om innskrevne emailer samsvarer, samt om noen av feltene er tomme
       //*Framtidige funksjoner: sjekke at emailen ikke allerede eksisterer
@@ -179,7 +396,9 @@ class CreateUser extends React.Component {
       }
       //Sjekker om innskrevne passord samsvarer, samt om noen av feltene er tomme
       //Famtidige funskjoner: implementer safe password (at det må være så pass langt og slikt)
-      if(isValidInput == true) {userService.addUser(this.refs.nFirstname.value, this.refs.nLastname.value, this.refs.nAddress.value, this.refs.nEmail1.value, this.refs.nPhonenumber.value, this.refs.nPassword1.value, (result) => {
+      if(isValidInput == true) {
+        history.replace("/userConfirmation/");
+        userService.addUser(this.refs.nFirstname.value, this.refs.nLastname.value, this.refs.nAddress.value, this.refs.nEmail1.value, this.refs.nPhonenumber.value, this.refs.nPassword1.value, (result) => {
 
         this.refs.nFirstname.value = "";
         this.refs.nLastname.value = "";
@@ -197,16 +416,68 @@ class CreateUser extends React.Component {
     };
   }
 }
+class UserConfirmation extends React.Component {
+  constructor() {
+    super(); // Call React.Component constructor
+
+    this.users = [];
+  }
+
+render() {
+  return (
+    <div>
+    <div>
+      <Link to="/">Tilbake til login</Link>
+    </div>
+    <div>
+    <h2>Takk for at du har meldt deg inn!</h2>
+    <h4>Du vil motta en email når søknadden din har blitt behandlet.</h4>
+    </div>
+    </div>
+    );
+  }
+}
+
+class PasswordConfirmation extends React.Component {
+  constructor() {
+    super(); // Call React.Component constructor
+
+    this.users = [];
+  }
+
+render() {
+  return (
+    <div>
+    <div>
+      <Link to="/">Tilbake til login</Link>
+    </div>
+    <div>
+    <h2>Passord tilbakestilt!</h2>
+    <h4>Du vil nå motta passordet til på din email.</h4>
+    </div>
+    </div>
+    );
+  }
+}
 
 ReactDOM.render((
   <HashRouter>
     <div>
-      <Menu />
       <Switch>
-        <Route exact path='/' component={LoginScreen} />
-        <Route exact path='/createUser/' component={CreateUser} />
-        <Route exact path='/profile/: userId' component={UserProfile} />
+        <Route exact path="/" component={LoginScreen} />
+        <Route exact path="/forgottenPassword/" component={ForgottenPassword} />
+        //<Route exact path="/passwordConfirmation/" component={PasswordConfirmation} />
+        <Route exact path="/createUser/" component={CreateUser} />
+        <Route exact path="/userConfirmation/" component={UserConfirmation} />
+        <Route exact path="/profile/:email" component={UserProfile} />
+        <Route exact path="/profile/:admin" component={UserProfileAdmin} />
+        <Route exact path="/arrangements/" component={Arrangements} />
+        <Route exact path="/changeProfile/" component={ChangeProfile} />
+        <Route exact path="/changeProfileSuccess/" component={ChangeProfileSuccess} />
+        <Route exact path="/arrangements/" component={Arrangements} />
+
       </Switch>
     </div>
   </HashRouter>
-), document.getElementById('root'));
+  //<Route exact path="/newUser/:email" component={NewUser} />
+), document.getElementById("root"));
--- src/services.js
+++ src/services.js
@@ -1,13 +1,13 @@
-import mysql from 'mysql';
+import mysql from "mysql";
 
 // Setup database server reconnection when server timeouts connection:
 let connection;
 function connect() {
   connection = mysql.createConnection({
-    host: 'mysql.stud.iie.ntnu.no',
-    user: '',
-    password: '',
-    database: ''
+    host: "mysql.stud.iie.ntnu.no",
+    user: "",
+    password: "",
+    database: ""
   });
 
   // Connect to MySQL-server
@@ -16,8 +16,8 @@ function connect() {
   });
 
   // Add connection error handler
-  connection.on('error', (error) => {
-    if (error.code === 'PROTOCOL_CONNECTION_LOST') { // Reconnect if connection to server is lost
+  connection.on("error", (error) => {
+    if (error.code === "PROTOCOL_CONNECTION_LOST") { // Reconnect if connection to server is lost
       connect();
     }
     else {
@@ -28,32 +28,97 @@ function connect() {
 connect();
 
 // Class that performs database queries related to customers
+class User {
+  constructor(firstName,lastName,address,email,phonenumber,password,points) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.email = email;
+    this.phonenumber = phonenumber;
+    this.password = password;
+    this.points = points;
+  }
+}
 class UserService {
-  getUser(callback) {
-    connection.query('SELECT * FROM Users', (error, result) => {
-      if (error) throw error;
-
-      callback(result);
+  signIn(inputEmail, inputPassword, callback) {
+      connection.query("SELECT * FROM Users WHERE email=? AND password=?", [inputEmail,inputPassword], (error, result) => {
+        let user = new User("No Result", "No Result", "No Result", "No Result", "No Result", "No Result", "No Result", "No Result");
+        if(result.length == 1) {
+          user = new User(result[0].firstName, result[0].lastName, result[0].address, result[0].email, result[0].phonenumber, result[0].password, 0);
+          localStorage.setItem("user", JSON.stringify(user));
+          console.log("successfully stored user");
+        }
+        callback(user);
+    });
+  }
+  getSignedInUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+  checkEmail(nEmail1, callback) {
+    console.log("Checking Email at Services side");
+    let exists = false;
+    connection.query("SELECT * FROM Users WHERE email=?", [nEmail1], (error, result) => {
+      if(result.length == 1) {
+        localStorage.setItem("exists", "true");
+      }
+      else{
+        localStorage.setItem("exists", "false");
+      }
+      callback();
     });
   }
 
-  getUser(id, callback) {
-    connection.query('SELECT * FROM Users WHERE id=?', [id], (error, result) => {
-      if (error) throw error;
+  isEmailTaken() {
+    return localStorage.getItem("exists") == true;
+  }
 
-      callback(result[0]);
+  userLogOut() {
+    localStorage.setItem("user","");
+    console.log("successfully removed user");
+  }
+  changeUserProfile(changeFirstName, changeLastName, changeAddress, changePhonenumber, email, password, callback) {
+    if(changeFirstName != "") {
+      connection.query("UPDATE Users SET firstName=? WHERE email=?", [changeFirstName, email], (error, result) => {
+        if (error) throw error;
+
+      });
+    } if(changeLastName != "") {
+      connection.query("UPDATE Users SET lastName=? WHERE email=?", [changeLastName, email], (error, result) => {
+        if (error) throw error;
+
+      });
+    } if(changeAddress != "") {
+      connection.query("UPDATE Users SET address=? WHERE email=?", [changeAddress, email], (error, result) => {
+        if (error) throw error;
+
+      });
+    } if(changePhonenumber != "" || !isNaN(changePhonenumber)) {
+      connection.query("UPDATE Users SET phonenumber=? WHERE email=?", [changePhonenumber, email], (error, result) => {
+        if (error) throw error;
+
+      });
+    }
+    this.signIn(email,password, (user) => {
+      console.log("Successfully signed in. User object following");
+      console.log(userService.getSignedInUser());
     });
+    callback();
   }
 
   addUser(nFirstname, nLastname, nAddress, nEmail1, nPhonenumber, nPassword1, callback) {
-    connection.query('INSERT INTO Users (firstName, lastName, address, email, phonenumber, password) values (?, ?, ?, ?, ?, ?)', [nFirstname, nLastname, nAddress, nEmail1, nPhonenumber, nPassword1], (error, result) => {
+    connection.query("INSERT INTO Users (firstName, lastName, address, email, phonenumber, password) values (?, ?, ?, ?, ?, ?)", [nFirstname, nLastname, nAddress, nEmail1, nPhonenumber, nPassword1], (error, result) => {
       if (error) throw error;
 
       callback();
     });
   }
+  newPassword(passwordEmail, callback) {
+    connection.query("SELECT * FROM Users WHERE email=? SET password =", [passwordEmail], (error, result) => {
+      if (error) throw error;
+
+    });
+  }
 }
 let userService = new UserService();
 
-export { UserService };
 export { userService };
